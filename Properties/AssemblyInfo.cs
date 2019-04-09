ff08f89fc71fbd1dafe1e752b9e684d410ec1cc9
Tom Francis <franc@tcaf.com>
2019-04-09 12:25:08 -0400

    Updated About dialog for automated builds
    Updated the About dialog to just show Application.ProductVersion again,
    which should match AssemblyInformationalVersion in
    Properties/AssemblyInof.cs.  Added simple value for
    AssemblyInformationalVersion to easily see if its replaced properly by
    the AppVeyor builds.  Also updated the AppVeyor configs to set
    AssemblyInformationalVersion appropriately.  Those configs do not yet
    appear in github.  They'll be added after verifying build successes.
    
    See issue #13.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.8.10.1")]
 [assembly: AssemblyFileVersion("1.8.10.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
