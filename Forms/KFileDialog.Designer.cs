fdf8da7cf875de8ac260cbc2e7832e5af8563dac
Tom Francis <franc@tcaf.com>
2020-03-19 15:13:06 -0400

    Fixed #49
    Modified dialog so all controls are in a split container, and resize
    appropriately.  Also added minimum sizes.

--- Forms/KFileDialog.Designer.cs
+++ Forms/KFileDialog.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+	    this.splitContainer = new System.Windows.Forms.SplitContainer();
             this.cancel = new System.Windows.Forms.Button();
             this.fileListView = new System.Windows.Forms.ListView();
             this.ok = new System.Windows.Forms.Button();
@@ -45,11 +46,13 @@
 	    this.viewSmallIcon = new System.Windows.Forms.MenuItem();
 	    this.viewList = new System.Windows.Forms.MenuItem();
 	    this.viewDetails = new System.Windows.Forms.MenuItem();
+            this.splitContainer.SuspendLayout();
             this.SuspendLayout();
             // 
             // cancel
             // 
-            this.cancel.Location = new System.Drawing.Point(431, 313);
+            this.cancel.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+	    this.cancel.Location = new System.Drawing.Point(326, 313);
             this.cancel.Name = "cancel";
             this.cancel.Size = new System.Drawing.Size(75, 23);
             this.cancel.TabIndex = 3;
@@ -59,10 +62,11 @@
             // 
             // fileListView
             // 
+            this.fileListView.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
             this.fileListView.HideSelection = false;
-            this.fileListView.Location = new System.Drawing.Point(110, 41);
+            this.fileListView.Location = new System.Drawing.Point(5, 41);
             this.fileListView.Name = "fileListView";
-            this.fileListView.Size = new System.Drawing.Size(399, 239);
+            this.fileListView.Size = new System.Drawing.Size(396, 239);
 	    this.fileListView.MultiSelect = false;
             this.fileListView.TabIndex = 7;
             this.fileListView.UseCompatibleStateImageBehavior = false;
@@ -72,7 +76,8 @@
             // 
             // ok
             // 
-            this.ok.Location = new System.Drawing.Point(431, 284);
+            this.ok.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            this.ok.Location = new System.Drawing.Point(326, 284);
             this.ok.Name = "ok";
             this.ok.Size = new System.Drawing.Size(75, 23);
             this.ok.TabIndex = 2;
@@ -82,11 +87,12 @@
             // 
             // lookIn
             // 
+            this.lookIn.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
 	    this.lookIn.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
 	    this.lookIn.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
 	    this.lookIn.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.lookIn.FormattingEnabled = true;
-            this.lookIn.Location = new System.Drawing.Point(110, 14);
+            this.lookIn.Location = new System.Drawing.Point(5, 14);
             this.lookIn.Name = "lookIn";
             this.lookIn.Size = new System.Drawing.Size(302, 21);
             this.lookIn.TabIndex = 5;
@@ -94,8 +100,9 @@
             // 
             // refresh
             // 
+            this.refresh.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
             this.refresh.Image = global::Ketarin.Properties.Resources.Restart;
-            this.refresh.Location = new System.Drawing.Point(474, 12);
+            this.refresh.Location = new System.Drawing.Point(369, 12);
             this.refresh.Name = "refresh";
             this.refresh.Size = new System.Drawing.Size(32, 23);
             this.refresh.TabIndex = 6;
@@ -103,6 +110,7 @@
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(59, 17);
             this.label1.Name = "label1";
@@ -112,9 +120,10 @@
             // 
             // extensions
             // 
+            this.extensions.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
 	    this.extensions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.extensions.FormattingEnabled = true;
-            this.extensions.Location = new System.Drawing.Point(182, 315);
+            this.extensions.Location = new System.Drawing.Point(77, 315);
             this.extensions.Name = "extensions";
             this.extensions.Size = new System.Drawing.Size(243, 21);
             this.extensions.TabIndex = 1;
@@ -122,12 +131,13 @@
             // 
             // fileName
             // 
+            this.fileName.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
 	    //this.fileName.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
 	    //this.fileName.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
 	    this.fileName.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
 	    this.fileName.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
 	    this.fileName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
-            this.fileName.Location = new System.Drawing.Point(182, 286);
+            this.fileName.Location = new System.Drawing.Point(77, 286);
             this.fileName.Name = "fileName";
             this.fileName.Size = new System.Drawing.Size(243, 20);
             this.fileName.TabIndex = 0;
@@ -136,6 +146,7 @@
             // 
             // mountList
             // 
+            this.mountList.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
             this.mountList.FormattingEnabled = true;
             this.mountList.Location = new System.Drawing.Point(12, 38);
             this.mountList.Name = "mountList";
@@ -145,8 +156,9 @@
             // 
             // viewButton
             // 
+            this.viewButton.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
             this.viewButton.Image = global::Ketarin.Properties.Resources.Symbol_Check;
-            this.viewButton.Location = new System.Drawing.Point(436, 12);
+            this.viewButton.Location = new System.Drawing.Point(322, 12);
             this.viewButton.Name = "viewButton";
             this.viewButton.Size = new System.Drawing.Size(32, 23);
             this.viewButton.TabIndex = 9;
@@ -155,8 +167,9 @@
             // 
             // fileNameLabel
             // 
+            this.fileNameLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
             this.fileNameLabel.AutoSize = true;
-            this.fileNameLabel.Location = new System.Drawing.Point(110, 289);
+            this.fileNameLabel.Location = new System.Drawing.Point(5, 289);
             this.fileNameLabel.Name = "fileNameLabel";
             this.fileNameLabel.Size = new System.Drawing.Size(55, 13);
             this.fileNameLabel.TabIndex = 10;
@@ -164,8 +177,9 @@
             // 
             // fileTypeLabel
             // 
+            this.fileTypeLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
             this.fileTypeLabel.AutoSize = true;
-            this.fileTypeLabel.Location = new System.Drawing.Point(110, 318);
+            this.fileTypeLabel.Location = new System.Drawing.Point(5, 318);
             this.fileTypeLabel.Name = "fileTypeLabel";
             this.fileTypeLabel.Size = new System.Drawing.Size(66, 13);
             this.fileTypeLabel.TabIndex = 11;
@@ -203,26 +217,43 @@
 	    this.viewDetails.Index = 0;
 	    this.viewDetails.Text = "Details";
 	    this.viewDetails.Click += new System.EventHandler(this.viewDetails_Click);
+	    //
+	    // splitContainer
+	    //
+	    this.splitContainer.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+	    this.splitContainer.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.splitContainer.Size = new System.Drawing.Size(526, 348);
+            this.splitContainer.SplitterDistance = 104;
+	    this.splitContainer.Panel1MinSize = 121;
+	    this.splitContainer.Panel2MinSize = 489;
+	    this.splitContainer.Panel1.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+	    this.splitContainer.Panel2.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
             // 
             // KFileDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(518, 348);
-            this.Controls.Add(this.fileTypeLabel);
-            this.Controls.Add(this.fileNameLabel);
-            this.Controls.Add(this.viewButton);
-            this.Controls.Add(this.mountList);
-            this.Controls.Add(this.fileName);
-            this.Controls.Add(this.extensions);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.refresh);
-            this.Controls.Add(this.lookIn);
-            this.Controls.Add(this.ok);
-            this.Controls.Add(this.fileListView);
-            this.Controls.Add(this.cancel);
+            this.ClientSize = new System.Drawing.Size(526, 348);
+	    // This is too small?!  But making it and/or ClientSize bigger
+	    // doesn't do what we want. :(
+	    this.MinimumSize = new System.Drawing.Size(526, 348);
+            this.splitContainer.Panel1.Controls.Add(this.label1);
+            this.splitContainer.Panel1.Controls.Add(this.mountList);
+            this.splitContainer.Panel2.Controls.Add(this.fileTypeLabel);
+            this.splitContainer.Panel2.Controls.Add(this.fileNameLabel);
+            this.splitContainer.Panel2.Controls.Add(this.viewButton);
+            this.splitContainer.Panel2.Controls.Add(this.fileName);
+            this.splitContainer.Panel2.Controls.Add(this.extensions);
+            this.splitContainer.Panel2.Controls.Add(this.refresh);
+            this.splitContainer.Panel2.Controls.Add(this.lookIn);
+            this.splitContainer.Panel2.Controls.Add(this.ok);
+            this.splitContainer.Panel2.Controls.Add(this.fileListView);
+            this.splitContainer.Panel2.Controls.Add(this.cancel);
+	    this.Controls.Add(this.splitContainer);
             this.Name = "KFileDialog";
             this.Text = "KFileDialog";
+            this.splitContainer.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -230,6 +261,7 @@
 
         #endregion
 
+        private System.Windows.Forms.SplitContainer splitContainer;
         private System.Windows.Forms.Button cancel;
         private System.Windows.Forms.ListView fileListView;
         private System.Windows.Forms.Button ok;
